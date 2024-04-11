866271c20074fe8f9155163f354fa2729a727345
Hemant <hemant@Hemants-MacBook-Air.local>
2024-04-11 18:43:42 +0530

    dashboard login mfe

---
+++ apps/employee/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { Route } from '@angular/router';
+import { loadRemoteModule } from '@nx/angular/mf';
+
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      loadRemoteModule('login', './Routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: '',
+    component: NxWelcomeComponent,
+  },
+];
